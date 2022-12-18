2bef850727a97a76fa31c7f832336f5639450003
SatoAkira <akira0418akira0418akira@gmail.com>
2022-12-18 15:58:30 +0900

    toppage作成

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <template>
-  <AppBar />
-  <CarouselView />
-  <NewsImg img="a" class="mb-10" />
-  <NewsImg img="a" class="mb-10" />
-  <CistName />
-  <FooterMenu />
+  <TopPage />
 </template>
 
 <script>
-//import HelloWorld from "./components/HelloWorld.vue";
-import CarouselView from "./components/CarouselView.vue";
-import AppBar from "./components/AppBar.vue";
-import NewsImg from "./components/NewsImg.vue";
-import FooterMenu from "./components/FooterMenu.vue";
-import CistName from "./components/CistName.vue";
-
+import TopPage from "./TopPage.vue";
 export default {
   name: "App",
   components: {
-    // HelloWorld,
-    CarouselView,
-    AppBar,
-    NewsImg,
-    CistName,
-    FooterMenu,
+    TopPage,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/TopPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <AppBar />
+  </div>
+  <div style="width: 100%; position: absolute; top: 0">
+    <CarouselView />
+    <NewsImg img="a" class="mb-10" />
+    <NewsImg img="a" class="mb-10" />
+    <CistName />
+    <FooterMenu />
+    <NewsView />
+  </div>
+</template>
+
+<script>
+import CarouselView from "./components/CarouselView.vue";
+import AppBar from "./components/AppBar.vue";
+import NewsImg from "./components/NewsImg.vue";
+import FooterMenu from "./components/FooterMenu.vue";
+import CistName from "./components/CistName.vue";
+
+export default {
+  name: "TopPage",
+  components: {
+    CarouselView,
+    AppBar,
+    NewsImg,
+    CistName,
+    FooterMenu,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
